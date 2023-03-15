95ca495b4cbb233951c9668c6398c5462aac1601
Lukas <lukepokorny@protonmail.com>
2023-03-15 11:43:37 +0100

    real object

--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,23 +1,36 @@
 import React from 'react';
 
 const UseStateObject = () => {
-  const [name, setName] = React.useState("peter")
-  const [age, setAge] = React.useState(24)
-  const [hobby, setHobby] = React.useState("reading books")
+  // const [name, setName] = React.useState("peter")
+  // const [age, setAge] = React.useState(24)
+  // const [hobby, setHobby] = React.useState("reading books")
+
+    // refactoring this ↑ to this ↓  
+
+  const [person, setPerson] = React.useState({
+    name:"peter",
+    age: 24,
+    hobby: "reading books",
+  });
 
   const displayPerson = () => {
-    setName("john")
-    setAge(28)
-    setHobby("screaming at the computer")
-  }  
+    // setName("john")
+    // setAge(28)
+    // setHobby("screaming at the computer")
 
-  return <>
-    <h3>{name}</h3>
-    <h4>{age}</h4>
-    <h5>Enjoys: {hobby}</h5>
+      // again
+      
+    setPerson({ name:"john", age:28, hobby:"screaming at the computer"});
+  };  
+
+  return (
+  <>
+    <h3>{person.name}</h3>
+    <h4>{person.age}</h4>
+    <h5>Enjoys: {person.hobby}</h5>
     <button className='btn' onClick={displayPerson}>Display</button>
   </>
 
-};
+)};
 
 export default UseStateObject;
