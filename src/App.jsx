c7a29a3e3b2508dc4dd85d04b4eb026dd85e994d
Lukas <lukepokorny@protonmail.com>
2023-03-15 17:03:31 +0100

    useState finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import Starter02 from './tutorial/01-useState/starter/02-useState-basics'
 import Starter03 from './tutorial/01-useState/starter/03-useState-array'
 import Starter04 from './tutorial/01-useState/starter/04-useState-object'
+import Starter05 from './tutorial/01-useState/starter/05-useState-gotcha'
+
 
 
 function App() {
   return (
     <div className='container'>
+      <Starter05 />
+      <br />
+      <br />
+      <br />
       <Starter04 />
       <br />
       <br />
       <br />
       <Starter03 />
+  
    
       
     </div>
--- src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,21 @@
+import React from 'react';
+
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+  const [num, setNum] = React.useState(0);
+
+  function add() {setNum(num + 1)}
+  function subtract() {setNum(num - 1)}
+  function zero(){setNum(num + 2)
+}
+  
+  return (
+    <>
+    <h2>{num}</h2>
+    <button className='btn' onClick={add}>+</button>
+    {/* <button className='btn' onClick={zero}>0</button> */}
+    <button className='btn' onClick={subtract}>-</button>
+    </>
+  )
 };
 
 export default UseStateGotcha;
